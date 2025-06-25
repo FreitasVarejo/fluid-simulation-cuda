49c52956aa7a5f0c551b725e96578e10754c1dde
freitaspinhe <gabrielpinheiro2003@gmail.com>
2025-06-25 15:21:20 -0300

    push do resto

---
+++ build/CMakeFiles/cuda.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/freitaspinhe/Desktop/unicamp/mc970/fluid-simulation-cuda/src/main_cuda.cu" "CMakeFiles/cuda.dir/src/main_cuda.cu.o" "gcc" "CMakeFiles/cuda.dir/src/main_cuda.cu.o.d"
+  "/home/freitaspinhe/Desktop/unicamp/mc970/fluid-simulation-cuda/src/solver_cuda.cu" "CMakeFiles/cuda.dir/src/solver_cuda.cu.o" "gcc" "CMakeFiles/cuda.dir/src/solver_cuda.cu.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
